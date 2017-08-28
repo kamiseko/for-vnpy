781799418756cfb7bf7322b1e336597946beaf33
kamiseko <kamiseko@gmail.com>
2017-08-28 17:32:42 +0800

    Add files via upload

--- modified tb data.py
+++ modified tb data.py
@@ -3,21 +3,12 @@
 
 # Libraries To Use
 from __future__ import division
-from CloudQuant import MiniSimulator
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
-import scipy.stats
 import os
-from sklearn import linear_model
 from datetime import datetime,time,date
-import matplotlib.pyplot as plt
-import seaborn as sns
-import time
 
-# Import My own library for factor testing
-from SingleFactorTest import factorFilterFunctions as ff
-#from config import *
 
 def tbDataToVnpy(filePath,fileName,savePath,newName):
     '''Adjust format of data downloaded from TradeBlazer to Vnpy data format.
@@ -26,18 +17,22 @@ def tbDataToVnpy(filePath,fileName,savePath,newName):
     fileName: String.Old file name for original csv data.
     savePath: String.new path for adjusted csv data.
     newName: String.new Name for adjusted csv data.'''
-    data = pd.read_csv(filePath+fileName,infer_datetime_format=True,header=None,names = ['Open', 'High', 'Low', 'Close','TotalVolume','Position'])
+    data = pd.read_csv(filePath+fileName,infer_datetime_format=True,header=None,names = ['Open', 'High', 'Low', 'Close','TotalVolume','TotalPosition'])
     data.index = data.index.map(lambda x : pd.to_datetime(x))
-    data['Time'] = data.index.map(lambda x : x.time)
-    data.index = data.index.map(lambda x : x.date)
+    data['Time'] = data.index.map(lambda x : x.time())
+    data.index = data.index.map(lambda x : x.date())
     data.index.name = 'Date'
-    data.drop('Position',axis=1,inplace = True)
+    #data.drop('Position',axis=1,inplace = True)
     data.to_csv(savePath+newName,na_rep='NaN',date_format='%Y%m%d')
     return data
 
 if __name__ =='__main__':
     old_path = 'C:/Users/LZJF_02/Desktop/original_data/'
     new_path = 'C:/Users/LZJF_02/Desktop/modified_data/'
-    filename = 'rb000_1min.csv'
-    newname = 'rb000_1min_modi.csv'
-    #c = tbDataToVnpy(old_path, filename, new_path, newname)
+    filename = 'if000_1min.csv'
+    newname = 'if000_1min_modi.csv'
+    c = tbDataToVnpy(old_path, filename, new_path, newname)
+    #for csvfile in os.listdir(old_path):
+     #   newname = csvfile.split('.')[0] +'_modi'+'.'+csvfile.split('.')[1]
+      #  c = tbDataToVnpy(old_path, csvfile, new_path, newname)
+       # print newname
